9878c9b58a173cfedba6a41b0e08ea8bda012db6
farhanparru <farhanparru87@gmail.com>
2024-02-07 13:33:08 -0500

    modifyi code

--- public/main.js
+++ public/main.js
@@ -8,6 +8,8 @@ const nameInput = document.getElementById('name-input')
 const messageForm = document.getElementById('message-form')
 const messageInput =  document.getElementById('message-input')
 
+const  messageTone = new Audio('/message-tone.mp3')
+
 messageForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     sendMessage()
@@ -18,16 +20,75 @@ socket.on('clients-total',(data)=>{
 })      
    
 function sendMessage(){
-    console.log(messageInput.value);
+    //Cannot send emit message back to the client
+    if(messageInput.value === '')return
+    // console.log(messageInput.value);
     const data = {
         name:nameInput.value,
         message:messageInput.value,
-        dateTime: new Date()
+        dateTime: new Date(),
     }
+  //message reciver
+    socket.emit('message', data)
+    addMessageToUI(true, data)
+    messageInput.value = ''
 
-    socket.emit('message',data)
 }     
 
 socket.on('chat-message',(data)=>{
-    console.log(data);
+    // console.log(data);
+    messageTone.play()
+    addMessageToUI(false, data)
 })
+
+function addMessageToUI(isOwnMessage,data){
+    clearFeedback()
+    const element = ` 
+    <li class="${isOwnMessage ? "message-right": "message-left"}">
+    <p class="message">
+        ${data.message}
+        <span>${data.name} 📅 ${moment(data.dateTime).fromNow()}</span>
+    </p>
+ </li>
+`
+messageContainer.innerHTML += element
+scrollToBottom()
+}
+
+function scrollToBottom(){
+    messageContainer.scrollTo(0,messageContainer.scrollHeight)
+}
+
+messageInput.addEventListener('focus',(e)=>{
+    socket.emit('feedback',{
+        feedback: `✍${nameInput.value} is typing a message`
+    })
+
+})
+
+messageInput.addEventListener('keypress',(e)=>{
+    socket.emit('feedback',{
+        feedback: `✍${nameInput.value} is typing a message`
+    })
+
+})
+messageInput.addEventListener('blur',(e)=>{
+    socket.emit('feedback',{
+        feedback: ``,
+    })
+})
+socket.on('feedback',(data)=>{
+    clearFeedback()
+    const element = ` 
+    <li class="message-feedback">
+    <p class="feedback" id="feedback">${data.feedback}</p>
+</li>
+`
+messageContainer.innerHTML += element
+})
+//clear Feddback
+function clearFeedback(){
+    document.querySelectorAll('li.message-feedback').forEach(element =>{
+        element.parentNode.removeChild(element)
+    })
+}
